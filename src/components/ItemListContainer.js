65e67f3c72549545b5f524fbbb88e53fd8e48f34
Pablo Groba <pgroba@gmail.com>
2022-02-09 12:02:23 -0300

    Counter

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -3,9 +3,9 @@ import { ItemCount } from './ItemCount';
 
 export const ItemListContainer = ({ greeting }) => {
   return (
-    <React.Fragment>
+    <>
       <h1 className="bg-lime-400 text-2xl p-2 m-2 text-center">{greeting}</h1>
       <ItemCount stock={5} initial={1} />
-    </React.Fragment>
+    </>
   );
 };
