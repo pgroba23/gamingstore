63d2679d31e5ad2dea70f00ace317d103d9bc94e
Pablo Groba <pgroba@gmail.com>
2022-02-09 11:59:30 -0300

    Counter

---
+++ src/components/ItemCount.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export const ItemCount = ({ stock, initial = 1 }) => {
+  const [count, setCount] = React.useState(initial);
+
+  const handleAdd = () => {
+    count < stock && setCount(count + 1);
+  };
+
+  const handleRemove = () => {
+    count > 0 && setCount(count - 1);
+  };
+
+  return (
+    <>
+      <div className="mx-2 mt-8 mb-4  text-center">
+        <button
+          className="py-2 px-4 bg-red-600 hover:bg-red-700 text-white font-bold rounded-sm"
+          onClick={handleRemove}
+        >
+          -
+        </button>
+        <span className="p-2 m-1 bg-zinc-200 text-lg rounded-sm">
+          Contador: {count}
+        </span>
+        <button
+          className="py-2 px-4 bg-green-600 hover:bg-green-700 text-white font-bold rounded-sm"
+          onClick={handleAdd}
+        >
+          +
+        </button>
+      </div>
+      <div className="m-2 text-center">
+        <button className="py-2 px-10 bg-slate-300 hover:bg-slate-700 text-white font-bold rounded-sm">
+          Agregar al carrito
+        </button>
+      </div>
+    </>
+  );
+};
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,5 +1,11 @@
+import React from 'react';
+import { ItemCount } from './ItemCount';
+
 export const ItemListContainer = ({ greeting }) => {
   return (
-    <h1 className="bg-lime-400 text-2xl p-2 m-2 text-center">{greeting}</h1>
+    <React.Fragment>
+      <h1 className="bg-lime-400 text-2xl p-2 m-2 text-center">{greeting}</h1>
+      <ItemCount stock={5} initial={1} />
+    </React.Fragment>
   );
 };
